8b0d68f0ed7908c170e35c7829bd933bf1f6ed3f
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-25 16:52:43 -0500

    Update tests to include test vector

--- EnigmaTests.cpp
+++ EnigmaTests.cpp
@@ -5,7 +5,24 @@ class EnigmaTest : public ::testing::Test
 {
 };
 
-TEST_F(EnigmaTest, EncryptDecryptConsistency)
+TEST_F(EnigmaTest, Encrypt)
+{
+    std::string key = "GBRXMD";
+    Enigma enigma(key);
+    std::vector<std::string> plaintext = {"THE", "QUICK", "BROWN", "FOX", "JUMPS", "OVER", "THE", "LAZY", "DOG"};
+    std::vector<std::string> expected = {"AJW", "OHMJM", "TPBAU", "HGV", "WYTRK", "MIIY", "VZC", "YEGA", "VMT"};
+    std::vector<std::string> encrypted;
+
+    enigma.Encrypt(plaintext, encrypted);
+
+    ASSERT_EQ(encrypted.size(), expected.size()); // Ensure the sizes are the same
+    for (size_t i = 0; i < plaintext.size(); ++i)
+    {
+        EXPECT_EQ(encrypted[i], expected[i]); // Compare each encrypted string with the expected string
+    }
+}
+
+TEST_F(EnigmaTest, EncryptDecryptRoundTrip)
 {
     std::string key = "KEY";
     Enigma enigma(key);

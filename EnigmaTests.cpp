0612d67142cd3be672894a73e45a124fe6c916db
Joel Carter <JoelBCarter@users.noreply.github.com>
2024-04-25 15:22:14 -0500

    Add tests

---
+++ EnigmaTests.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+#include "Enigma.h"
+
+class EnigmaTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        // Setup code here, if necessary
+    }
+
+    void TearDown() override {
+        // Cleanup code here, if necessary
+    }
+};
+
+TEST_F(EnigmaTest, EncryptDecryptConsistency) {
+    std::string key = "KEY";
+    Enigma enigma(key);
+    std::vector<std::string> plaintext = {"HELLO", "WORLD"};
+    std::vector<std::string> encrypted;
+    std::vector<std::string> decrypted;
+
+    enigma.Encrypt(plaintext, encrypted);
+    enigma.Decrypt(encrypted, decrypted);
+
+    ASSERT_EQ(plaintext.size(), decrypted.size());
+    for (size_t i = 0; i < plaintext.size(); ++i) {
+        EXPECT_EQ(plaintext[i], decrypted[i]);
+    }
+}
+
+TEST_F(EnigmaTest, EncryptDecryptNonAlphaCharacters) {
+    std::string key = "KEY";
+    Enigma enigma(key);
+    std::vector<std::string> plaintext = {"HELLO 123", "WORLD!@#"};
+    std::vector<std::string> encrypted;
+    std::vector<std::string> decrypted;
+
+    enigma.Encrypt(plaintext, encrypted);
+    enigma.Decrypt(encrypted, decrypted);
+
+    ASSERT_EQ(plaintext.size(), decrypted.size());
+    for (size_t i = 0; i < plaintext.size(); ++i) {
+        EXPECT_EQ(plaintext[i], decrypted[i]);
+    }
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
